adf93331cbaa7807cec7d9dac19b07d5ce9c67ba
Rudenjer <denjers@yandex.ru>
2016-10-30 01:29:58 +0300

    Edit views in Admin area

--- HotelData/Context/DataBaseInitilizer.cs
+++ HotelData/Context/DataBaseInitilizer.cs
@@ -45,7 +45,8 @@ namespace HotelData.Context
 
                 new Record {ClientID="1", DateIn= new DateTime(2016,3,14), DateOut = new DateTime(2016,3,16)},
                 new Record {ClientID="2", DateIn=new DateTime(2016,4,15), DateOut= new DateTime(2016,4,20) },
-                new Record {ClientID="2", DateIn=DateTime.Now.AddDays(-3),  DateOut= DateTime.Now.AddDays(3)  }
+                new Record {ClientID="2", DateIn=DateTime.Now.AddDays(-3),  DateOut= DateTime.Now.AddDays(3)  },
+                new Record {ClientID="2", DateIn=DateTime.Now.AddDays(12),  DateOut= DateTime.Now.AddDays(17)}
             };
 
             List<Record> records2 = new List<Record>
@@ -53,7 +54,8 @@ namespace HotelData.Context
 
                 new Record {ClientID="1", DateIn= new DateTime(2015,10,1), DateOut = new DateTime(2015,10,5)},
                 new Record {ClientID="3", DateIn=new DateTime(2015,11,3), DateOut = new DateTime(2015,11,8)},
-                new Record {ClientID="2", DateIn=DateTime.Now.AddDays(-2),  DateOut= DateTime.Now.AddDays(2)}
+                new Record {ClientID="2", DateIn=DateTime.Now.AddDays(-2),  DateOut= DateTime.Now.AddDays(2)},
+                new Record {ClientID="2", DateIn=DateTime.Now.AddDays(10),  DateOut= DateTime.Now.AddDays(15)}
             };
 
 
